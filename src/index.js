236ba3fb6ee2c7cd98892d29a446baf36a30e627
dhanisthlm <dhanisthlm@gmail.com>
2017-08-29 19:10:45 +0200

    index update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { compose, createStore, combineReducers, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import { Router, Route, IndexRedirect, browserHistory } from 'react-router'
-import { syncHistory } from 'redux-simple-router'
-import thunkMiddleware from 'redux-thunk'
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRedirect, browserHistory } from 'react-router';
+import { syncHistory } from 'redux-simple-router';
+import thunkMiddleware from 'redux-thunk';
 
-import reducers from './reducers'
-import App from './components/App'
-import Contract from './components/Contract'
+import reducers from './reducers';
+import App from './components/App';
+import Contract from './components/Contract';
 
-const reduxRouterMiddleware = syncHistory(browserHistory)
-const createStoreWithMiddleware = applyMiddleware(reduxRouterMiddleware, thunkMiddleware)(createStore)
-const store = createStoreWithMiddleware(reducers)
+const reduxRouterMiddleware = syncHistory(browserHistory);
+const createStoreWithMiddleware = applyMiddleware(reduxRouterMiddleware, thunkMiddleware)(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 render((
   <Provider store={store}>
